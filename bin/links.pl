606c26a71ee85be5e7c7289379c8738eacdefe09
Oliver Gorwits <oliver@cpan.org>
2015-02-06 09:22:30 +0000

    fix undef when no link

--- bin/links.pl
+++ bin/links.pl
@@ -12,7 +12,7 @@ my $list = 'http://ediblereading.com/the-list-alphabetical/';
 my @links = $ua->get($list)->res->dom->find('div.post-entry a')->each;
 
 foreach my $l (@links) {
-    my $link = $l->attr('href');
+    my $link = $l->attr('href') or next;
     next if $seen->{$link}++;
     next if $link !~ m!/\d{4}/\d{2}/\d{2}/!;
     print $link, "\n";
