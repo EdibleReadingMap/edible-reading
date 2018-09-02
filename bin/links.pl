3edc2136c5893456d04d1cd2da7a157de2ec46e5
Oliver Gorwits <oliver@cpan.org>
2018-09-02 21:18:20 +0100

    force https

--- bin/links.pl
+++ bin/links.pl
@@ -15,5 +15,7 @@ foreach my $l (@links) {
     my $link = $l->attr('href') or next;
     next if $seen->{$link}++;
     next if $link !~ m!/\d{4}/\d{2}/\d{2}/!;
+    $link =~ s|^https?://||;
+    $link = 'https://' . $link;
     print $link, "\n";
 }
