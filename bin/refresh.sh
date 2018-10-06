e34ee5cf6ae3509b62aa9283865699d2a2fc39a3
Oliver Gorwits <oliver@cpan.org>
2018-10-06 22:51:01 +0100

    fix accidental API commit

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read line;
-do echo "$line" | ./parse.pl | GOOGLE_API_KEY='AIzaSyA15ABQUyKuW2f2_ICySTXWzP-iXL_m2P4' ./google.pl | ./save.pl;
+do echo "$line" | ./parse.pl | ./google.pl | ./save.pl;
 done;
