5e1063a43fa4eee44bba5b2dd551c0f5d9b872b8
Oliver Gorwits <oliver@cpan.org>
2018-10-14 12:24:19 +0100

    add oishi and faster links script

---
+++ bin/links-bytime-quick.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Mojo::UserAgent;
+
+my $seen = {};
+my $ua = Mojo::UserAgent->new;
+
+foreach my $year (18) {
+  foreach my $month ('09' .. 12) {
+    my $list = "https://ediblereading.com/20$year/$month/";
+    my $page = $ua->get($list) or next;
+    my @links = $page->res->dom->find('h1.entry-title a')->each;
+
+    foreach my $l (@links) {
+        my $link = $l->attr('href') or next;
+        next if $seen->{$link}++;
+        next if $link !~ m!/\d{4}/\d{2}/\d{2}/!;
+
+        my $review = $ua->get($link) or next;
+        my $cat = $review->res->dom->find('span.cat-links a')->first;
+        next unless $cat and $cat->text eq 'Reviews';
+
+        $link =~ s|^https?://||;
+        print $link, "\n";
+    }
+  }
+}
